20faa2d14842bc14802574259742718bfc49b4f9
Paweł Koper <56790450+pol1991@users.noreply.github.com>
2021-10-17 15:33:00 +0200

    fourth commit

--- functions.php
+++ functions.php
@@ -3,24 +3,29 @@
 * Theme Functions
 *
 *@package Gold Train
+*
 */
 
 
 function gold_train_enqueue_scripts() {
 
     /** Register Styles */
+    wp_register_style('bootstrap', get_template_directory_uri().'/assets/src/bootstrap/css/bootstrap.min.css', [] , false, 'all');
     wp_register_style('theme-variables', get_template_directory_uri().'/assets/css/theme-variables.css', [], filemtime( get_template_directory().'/assets/css/theme-variables.css'), 'all');
     wp_register_style('style', get_stylesheet_uri(), ['theme-variables'], filemtime( get_template_directory().'/style.css' ), 'all' );
 
     /** Register Scripts */
-    wp_register_script('main-js', get_template_directory_uri().'/assets/js/main.js', [], filemtime( get_template_directory().'/assets/js/main.js' ),true );
+    wp_register_script('main', get_template_directory_uri().'/assets/js/main.js', [], filemtime( get_template_directory().'/assets/js/main.js' ),true );
+    wp_register_script('bootstrap', get_template_directory_uri().'/assets/src/bootstrap/js/bootstrap.min.js', ['jquery'] , false, true);
 
     /** Enqueue Styles */
+    wp_enqueue_style('bootstrap');
     wp_enqueue_style('style');
     wp_enqueue_style('theme-variables');
 
     /** Enqueue Scripts */
-    wp_enqueue_script('main-js');
+    wp_enqueue_script('main');
+    wp_enqueue_script('bootstrap');
 
     /**
      * wp_register_style('style', get_stylesheet_uri(), ['theme-variables'], filemtime( get_template_directory().'/style.css' ), 'all' );
--- header.php
+++ header.php
@@ -14,8 +14,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(''); ?></title>
     <?php wp_head(); ?>
-    
+
 </head>
 <body <?php body_class(); ?> >
-<?php wp_body_open(); ?>
-<header>Nagłówek</header>
+<?php 
+if( function_exists('wp_body_open') ){
+    wp_body_open();
+}
+ ?>
+
+<div id="page" class="site">
+<header id="master-header" class="site-header" role="banner">
+    <?php get_template_part('template-parts/header/nav'); ?>
+</header>
+<div id="content" class="site-content">
+
+</div>
+</div>
