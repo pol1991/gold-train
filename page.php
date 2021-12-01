f2a178541e3b52654cd6d6bf7ccd5fa17ee873f8
Paweł Koper <56790450+pol1991@users.noreply.github.com>
2021-12-01 20:49:30 +0100

    theme options

--- page.php
+++ page.php
@@ -7,7 +7,16 @@
 */
 get_header();
 ?>
-<div class="row">Single Page Template</div>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="container">
+        <?php while (have_posts()) : the_post(); ?>
+        <?php the_content(''); ?>
+        <?php endwhile; ?>
+        </div>
+    </main>
+</div>
 
 <?php 
 get_footer();
